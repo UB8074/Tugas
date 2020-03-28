806dbe1ff14638f5b88cd3a285ac90fc0e6f7709
Rasyid Hidayatullah <arrasyidtz3@gmail.com>
2020-03-28 11:19:47 +0700

    Bismillahirrahmanirrahim

--- app/src/main/java/com/example/tugas/MainActivity.java
+++ app/src/main/java/com/example/tugas/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.tugas;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -20,21 +20,21 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     RadioGroup RG1, RG2, RG3, RG4, RGjk;
     RadioButton lk, pr, islam, kristen, katolik, hindu, buddha, konghucu, aliran;
-    EditText ETnamaDpn, ETnamaBlkng, ETtmptL, ETdate, ETalmt, ETtlp, ETemail, ETpass;
+    EditText ETnamaDpn, ETnamaBlkng, ETtmptL, ETdate, ETalmt, ETtlp, ETemail, ETpass, ETpassUlang;
     Button Btndaftar, Btnkembali, BtnOke, BtnKeluar;
 
     String Bc, jk, agama, namaDpn, namaBlkng,
-            tempat, tanggal, alamat, telpon, email, password, pass;
+            tempat, tanggal, alamat, telpon, email, password, passUlang;
     TextView TvnamaDpn, TvnamaBlkng, TvTTL, Tvalamat, Tvjk, Tvagama, Tvtlp, Tvemail;
 
     int tahun, bulan, hari;
-    static final int DIALOG_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         ETtlp = findViewById(R.id.ETtlp);
         ETemail = findViewById(R.id.ETemail);
         ETpass = findViewById(R.id.ETpass);
+        ETpassUlang = findViewById(R.id.ETpassUlang);
 
         RGjk = findViewById(R.id.RGjk);
         RG1 = findViewById(R.id.RG1);
@@ -70,37 +71,41 @@ public class MainActivity extends AppCompatActivity {
         Btnkembali = findViewById(R.id.Btnkembali);
 
         Bc = BuildConfig.FLAVOR;
+        ETemail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-        Btndaftar.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!ETemail.getText().toString().trim()
+                        .matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")) {
+                    ETemail.setError("Format Email Salah");
+                }
+            }
+        });
+
+        ETdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePick();
+            }
+        });
+        Btndaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 isValidEditText();
                 isValidPassword();
             }
         });
-
         Btnkembali.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
-            }
-        });
-        showDialogOnEditText();
-
-        ETemail.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                isValidEmail(s);
             }
         });
     }
@@ -115,9 +120,11 @@ public class MainActivity extends AppCompatActivity {
         telpon = ETtlp.getText().toString();
         email = ETemail.getText().toString();
         password = ETpass.getText().toString();
+        passUlang = ETpass.getText().toString();
 
         if (namaDpn.isEmpty() && namaBlkng.isEmpty() && tempat.isEmpty() && tanggal.isEmpty() &&
-                alamat.isEmpty() && telpon.isEmpty() && email.isEmpty() && password.isEmpty()) {
+                alamat.isEmpty() && telpon.isEmpty() && email.isEmpty() && password.isEmpty() &&
+                passUlang.isEmpty()) {
             ETnamaDpn.setError("Nama Depan Harus Diisi");
             ETnamaBlkng.setError("Nama Belakang Harus Diisi");
             ETtmptL.setError("Tempat Lahir Harus Diisi");
@@ -126,20 +133,14 @@ public class MainActivity extends AppCompatActivity {
             ETtlp.setError("Nomor Telepon Harus Diisi");
             ETemail.setError("Email Harus Diisi");
             ETpass.setError("Password Harus Diisi");
+            ETpassUlang.setError("Password Harus Diisi");
         } else {
             konfirmasi();
         }
     }
 
-    public void isValidEmail(Editable s) {
-        email = ETemail.getText().toString().trim();
-        if (!email.matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")) {
-            ETemail.setError("Format Email Salah");
-        }
-    }
-
-    public boolean pattern(String pass2) {
-        Bc = "^(?=.*[0-9])" +
+    public boolean pattern(String pass) {
+        String patt = "^(?=.*[0-9])" +
                 "(?=.*[a-z])" +
                 "(?=.*[A-Z])" +
                 "(?=.*[@#$%^&+=])" +
@@ -151,33 +152,34 @@ public class MainActivity extends AppCompatActivity {
                 "(?=.*[A-Z])" +
                 "(?=.*[@#$%^&+=])" +
                 "(?=\\S+$).{4,}" +
-                "$").matcher(pass2).matches();
+                "$").matcher(pass).matches();
     }
 
     public void isValidPassword() {
-        password = ETpass.getText().toString().trim();
-        if (TextUtils.isEmpty(password) || password.length() < 8) {
-            ETpass.setError("Panjang password minimal adalah 8 karakter dengan angka, huruf " +
-                    "besar, huruf kecil, dan spesial karakter (seperti: @, &amp;, #, ?)");
-        } else if (!pattern(password)) {
-            ETpass.setError("Panjang password minimal adalah 8 karakter dengan angka, huruf " +
-                    "besar, huruf kecil, dan spesial karakter (seperti: @, &amp;, #, ?)");
+        String err = "Panjang password minimal adalah 8 karakter dengan angka, huruf " +
+                "besar, huruf kecil, dan spesial karakter (seperti: @,&,#,?)";
+        if (TextUtils.isEmpty(ETpass.getText().toString().trim()) ||
+                ETpass.getText().toString().trim().length() < 8) {
+            ETpass.setError(err);
+        } else if (!pattern(ETpass.getText().toString().trim())) {
+            ETpass.setError(err);
         } else {
-            if (ETpass.getText().toString().isEmpty()) {
-                ETpass.setError("Password harus sama");
-            } else if (!password.equals(ETpass.getText().toString().trim())) {
-                ETpass.setError("Password harus sama");
+            if (!ETpass.getText().toString().trim().equals(
+                    ETpassUlang.getText().toString().trim())) {
+                ETpassUlang.setError("Password harus sama");
             }
         }
     }
 
     //-----------------------------------------View Hasil-------------------------------------------
     public void HasilPendaftaran() {
-        final AlertDialog alertDialogBuilder = new AlertDialog.Builder(this).create();
-        View dialogView = getLayoutInflater().inflate(R.layout.hasil_pendaftaran, null);
-        alertDialogBuilder.setTitle("Detail Pendaftaran");
-        alertDialogBuilder.setView(dialogView);
-        alertDialogBuilder.setIcon(R.mipmap.ic_launcher);
+        final AlertDialog aDB = new AlertDialog.Builder(this).create();
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.hasil_pendaftaran, null);
+        aDB.setTitle("Detail Pendaftaran");
+        aDB.setView(dialogView);
+        aDB.setCancelable(true);
+        aDB.setIcon(R.mipmap.ic_launcher);
 
         TvnamaDpn = dialogView.findViewById(R.id.TvnamaDpn);
         TvnamaBlkng = dialogView.findViewById(R.id.TvnamaBlkng);
@@ -199,91 +201,102 @@ public class MainActivity extends AppCompatActivity {
         Tvtlp.setText(telpon);
         Tvjk.setText(jk);
 
-        TextView tv = TvTTL;
-        StringBuilder sb = new StringBuilder();
-        sb.append(tempat);
-        sb.append(", ");
-        sb.append(tanggal);
-        tv.setText(sb.toString());
-
+        StringBuilder sB = new StringBuilder();
+        sB.append(tempat);
+        sB.append(", ");
+        sB.append(tanggal);
+        TvTTL.setText(sB.toString());
 
         BtnOke.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Pendaftaran Selesai",
                         Toast.LENGTH_LONG).show();
-                alertDialogBuilder.dismiss();
+                aDB.dismiss();
             }
         });
+
         BtnKeluar.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 finish();
             }
         });
-        alertDialogBuilder.show();
+
+        aDB.show();
     }
 
     //    ------------------------------------konfirmasi--------------------------------------------
     private void konfirmasi() {
-        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setMessage("Apakah data yang anda masukkan sudah benar?");
-        alertDialogBuilder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+        final AlertDialog.Builder aDB = new AlertDialog.Builder(this);
+        aDB.setTitle("Detail Pendaftaran");
+        aDB.setCancelable(true);
+        aDB.setIcon(R.mipmap.ic_launcher);
+        aDB.setMessage("Apakah data yang anda masukkan sudah benar?");
+
+        aDB.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 HasilPendaftaran();
             }
         });
-        alertDialogBuilder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+
+        aDB.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
         });
 
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
+        AlertDialog aD = aDB.create();
+
+        aD.show();
     }
 
-    //    ------------------------------------Pilih tanggal-----------------------------------------
-    public void showDialogOnEditText() {
+    //----------------------------------------Pilih tanggal-----------------------------------------
+    public void datePick() {
+        Calendar c = Calendar.getInstance();
+        tahun = c.get(Calendar.YEAR);
+        bulan = c.get(Calendar.MONTH);
+        hari = c.get(Calendar.DAY_OF_MONTH);
 
-        ETdate.setOnClickListener(
-                new OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showDialog(DIALOG_ID);
+        DatePickerDialog dPD = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker view, int _tahun, int _bulan, int _hari) {
+                        ETdate.setText(_hari + "-" + (_bulan + 1) + "-" + _tahun);
                     }
-                }
+                }, tahun, bulan, hari
         );
+
+        dPD.show();
     }
 
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        if (id == DIALOG_ID)
-            return new DatePickerDialog(this, Listner, tahun, bulan, hari);
-        return null;
-    }
-
-    private final DatePickerDialog.OnDateSetListener Listner =
-            new DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker view, int thn, int bln, int hr) {
-                    tahun = thn;
-                    bulan = bln;
-                    hari = hr;
-                    ETdate.setText(hari + "-" + bulan + "-" + tahun);
-                }
-            };
-
-    //--------------------------------------Radio Button--------------------------------------------
-    public void OnClickRB(View v) {
+    //----------------------------------Choose Radio Button-----------------------------------------
+    public void ChooseRB(View v) {
         boolean checked = ((RadioButton) v).isChecked();
 
         switch (v.getId()) {
-            case R.id.buddha:
+            case R.id.lk:
                 if (checked) {
-                    agama = "Buddha";
-                    RG1.clearCheck();
+                    jk = "Laki-Laki";
+                }
+                break;
+            case R.id.pr:
+                if (checked) {
+                    jk = "Perempuan";
+                }
+                break;
+            case R.id.islam:
+                if (checked) {
+                    agama = "Islam";
                     RG2.clearCheck();
+                    RG3.clearCheck();
+                    RG4.clearCheck();
+                }
+                break;
+            case R.id.kristen:
+                if (checked) {
+                    agama = "Kristen";
+                    RG2.clearCheck();
+                    RG3.clearCheck();
                     RG4.clearCheck();
                 }
                 break;
@@ -295,11 +308,11 @@ public class MainActivity extends AppCompatActivity {
                     RG4.clearCheck();
                 }
                 break;
-            case R.id.islam:
+            case R.id.buddha:
                 if (checked) {
-                    agama = "Islam";
+                    agama = "Buddha";
+                    RG1.clearCheck();
                     RG2.clearCheck();
-                    RG3.clearCheck();
                     RG4.clearCheck();
                 }
                 break;
@@ -319,30 +332,12 @@ public class MainActivity extends AppCompatActivity {
                     RG4.clearCheck();
                 }
                 break;
-            case R.id.kristen:
-                if (checked) {
-                    agama = "Kristen";
-                    RG2.clearCheck();
-                    RG3.clearCheck();
-                    RG4.clearCheck();
-                }
-                break;
             case R.id.aliran:
                 if (checked) {
                     agama = "Aliran Kepercayaan";
                     RG1.clearCheck();
                     RG2.clearCheck();
                     RG3.clearCheck();
-                }
-                break;
-            case R.id.lk:
-                if (checked) {
-                    jk = "Laki-Laki";
-                }
-                break;
-            case R.id.pr:
-                if (checked) {
-                    jk = "Perempuan";
                 }
                 break;
             default:
